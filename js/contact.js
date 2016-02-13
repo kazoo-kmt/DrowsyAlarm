4b800b922ed72c58dce05ed47c45c23b75dcf82c
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2016-02-13 15:09:29 -0800

    Try to use Formspree because github pages don't allow to use php contact form

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+
+    $('form').submit(function(event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: "http://formspree.io/drowsyalarm@gmail.com",
+            method: "POST",
+            data: {
+                name: $('#js-name').val(),
+                email: $('#js-email').val(),
+                subject: $('#js-subject').val(),
+                message: $('#js-message').val()
+            },
+            dataType: "json"
+        }).error(function(jqXHR, status, error){
+            alert('Uh oh, something went wrong. Please try again.');
+        }). success(function(data, status, jqXHR) {
+            alert('Thank you for your message. Kitty will get back to you soon.');
+            $('form').find('#js-name, #js-email, #js-subject, #js-message').val('');
+        });
+    });
+
+});
