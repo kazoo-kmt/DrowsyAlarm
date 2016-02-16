38a05202fc1e52d02ad015c1d9d673b2f2c336f2
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2016-02-15 16:16:24 -0800

    Modified mailform

--- js/contact.js
+++ js/contact.js
@@ -14,9 +14,9 @@ $( document ).ready(function() {
             },
             dataType: "json"
         }).error(function(jqXHR, status, error){
-            alert('Uh oh, something went wrong. Please try again.');
+            alert('Something went wrong. Please try again.');
         }). success(function(data, status, jqXHR) {
-            alert('Thank you for your message. Kitty will get back to you soon.');
+            alert('Thank you for your message. We will get back to you soon.');
             $('form').find('#js-name, #js-email, #js-subject, #js-message').val('');
         });
     });
